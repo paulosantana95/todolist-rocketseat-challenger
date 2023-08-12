da06235e51d72fd5b41a35fff7f30beb524ec692
Paulo Santana <paulosantana.sa@gmail.com>
2023-08-12 00:48:07 -0300

    style: adjust todo list layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-// import styles from "./App.module.css";
-import { Header } from "./components/Header";
+import styles from "./App.module.css";
 import "./global.css";
+
+import { Header } from "./components/Header";
+import { TodoList } from "./components/TodoList";
 
 function App() {
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <TodoList />
+      </div>
     </div>
   );
 }
